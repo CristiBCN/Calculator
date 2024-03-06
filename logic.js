b0b423dec5ceb771fcc94c373593a6b1a4875088
CristiBCN <cristidnc72@gmail.com>
2024-03-06 15:09:25 +0100

    Updated number's displaying and operating
    Created displayNumber function.
    First attempt to create calculator's operations code

--- logic.js
+++ logic.js
@@ -1,9 +1,44 @@
+//let the numbers show:
+
+let display = document.querySelector('#display');
+let numbers = document.querySelectorAll('div.numbers > button');
+
+function toggleSign() {  
+  display.value = -display.value;  
+}
+
+function setDecimal() {
+  display.value.includes('.') ? '' : 
+    (display.value === '0' ||display.value === '')
+      ? display.value = '0' + '.' 
+      : display.value = display.value + '.';
+}
+
+function handlerDisplay(e) {
+  display.value !== '0' 
+    ? display.value += e.target.id        
+    : display.value = e.target.id;
+  //return display.value; 
+}
+
+function displayNumber() {
+  for (let number of numbers) {
+    if (number.id === 'sign') {
+      number.addEventListener("click", toggleSign);
+    } else if (number.id === 'dec') { 
+      number.addEventListener("click", setDecimal);
+    } else {
+      number.addEventListener("click", handlerDisplay);
+    }  
+  }
+ 
+}
+
+displayNumber();
+
 //define variables:
 
-let n1 = 0;
-let n2 = 0;
-let operator;
-
+let n1, n2, operator;
 
 //define the operators and operation:
 
@@ -23,57 +58,67 @@ function divide(a, b) {
   return a / b;
 }
 
+
+//store the numbers and operate:
+
+let plus = document.querySelector('#adds');
+let minus = document.querySelector('#subtracts');
+let times = document.querySelector('#multiplies');
+let by = document.querySelector('#divides');
+let equal = document.querySelector('#equals');
+
+/*function setNumber(e) {
+  let num = +display.value;
+  (e.target === plus || e.target === minus || e.target === times || e.target === by) 
+    ? display.value = '0' : '';
+  return +num;  
+}*/
+
 function operate(operator, a, b) {
   switch (operator) {
-    case add:
-      add(a, b);
-      break;
-    case subtract:
-      subtract(a, b);
-      break;
-    case multiply:
-      multiply(a, b);
-      break;
-    case divide:
-      divide(a, b);
-      break;    
+    case 'adds':
+      return add(a, b);      
+    case 'subtracts':
+      return subtract(a, b);      
+    case 'multiplies':
+      return multiply(a, b);      
+    case 'divides':
+      return divide(a, b);          
   }
 }
 
+function handlerOperations(e) {
+  n1 = +display.value;
+  //display.value = '';
+  //handlerDisplay(e);
+  console.log(n1);
 
-//let yhe numbers show:
-
-let display = document.querySelector('#display');
-let numbers = document.querySelectorAll('div.numbers > button');
-console.log(numbers);
-
-
-function toggleSign() {  
-  display.value = -display.value;
-}
-
-function setDecimal() {
-  display.value.includes('.') ? '' : 
-    (display.value === '0' ||display.value === '')
-      ? display.value = '0' + '.' 
-      : display.value = display.value + '.';
-}
-
-function handler(e) {
-  display.value !== '0' 
-    ? display.value += e.target.id        
-    : display.value = e.target.id; 
-  console.log(display.value);
-}
-
-for (let number of numbers) {
-  if (number.id === 'sign') {
-    number.addEventListener("click", toggleSign);
-  } else if (number.id === 'dec') { 
-    number.addEventListener("click", setDecimal);
+  console.log(e.target.id);
+  operator = e.target.id;
+  if (operator) {    
+    
+    n2 = displayNumber();
+  }
+  console.log(n2);
+  
+/*
+    n2 = operate(operator, n1, +display.value);
+    display.value = n1;
+    console.log(n1);
+    return n1;
   } else {
-    number.addEventListener("click", handler);
-  }
+    return n1 = operate(operator, n1, +display.value)
+  }*/
+  
+  //n1 ? n1 = operate(operator, n1, setNumber(e)) : n1 = setNumber(e);
+  
+  
+  //n2 = setNumber(e);
+  //display.value = operate(e.target.id, n1, n2);
+    
 }
 
-
+plus.addEventListener('click', handlerOperations);
+minus.addEventListener('click', handlerOperations);
+times.addEventListener('click', handlerOperations);
+by.addEventListener('click', handlerOperations);
