33fb9b84b12ae8ec937d84d68e71b54f931c3730
CristiBCN <cristidnc72@gmail.com>
2024-03-21 17:25:49 +0100

    A functional variant
    I should to add style and check-up for bugs

--- logic.js
+++ logic.js
@@ -1,124 +1,154 @@
-//let the numbers show:
+const display = document.querySelector('#display');
+const buttons = document.querySelector('.buttons');
+const MC = document.querySelector('#memoryClear');
+const MR = document.querySelector('#memoryRecall');
 
-let display = document.querySelector('#display');
-let numbers = document.querySelectorAll('div.numbers > button');
+let n1 = null,   
+  n2 = null,
+  //num = null, 
+  operator = null,  
+  //calculated = false,
+  mem = 0,
+  res = null;
+
+const operate = {
+  adds: (a, b) => a + b,
+  subtracts: (a, b) => a - b,
+  multiplies: (a, b) => a * b,
+  divides: (a, b) => a / b,
+}
+
+buttons.addEventListener("click", (e) => userHandler(e));
+
+let inserting = false;
+
+function userHandler(e) {
+  const userInput = e.target.value ? e.target.value : e.target.id;
+  console.log(userInput);
+
+  !inserting ? display.value = 0 : '';
+  
+  if (e.target.value) {
+    handlerDisplay(e);
+    inserting = true;    
+  } else if (userInput === "dec") {
+    setDecimal();
+    inserting = true;
+  } else if (userInput === "sign") {
+    toggleSign();  
+  } else if (Object.keys(operate).includes(userInput)) {
+    if (!n1) {
+      n1 = Number(display.value);
+    } else {
+      n2 = Number(display.value);
+      res = calculate(n1, operator, n2);
+      display.value = res;
+      n1 = res;
+      //n2 = null;
+    }
+    operator = userInput;
+    inserting = false;
+  } else if (userInput === "clear") {
+    clearAll();
+  } else if (userInput === "del") {
+    del();
+  } else if (userInput === "equal" && operator) {
+    n2 = Number(display.value);
+    res = calculate(n1, operator, n2);
+    display.value = res;
+    n1 = res;
+    inserting = false;
+  } else if (userInput === "memoryStore") {
+    mem = Number(display.value);
+    inserting = false;
+    MC.disabled = false;
+    MR.disabled = false;    
+  } else if (userInput === "memoryMinus") {
+    mem = mem - Number(display.value);
+    inserting = false;
+    MC.disabled = false;
+    MR.disabled = false;    
+  } else if (userInput === "memoryPlus") {
+    mem = mem + Number(display.value);
+    inserting = false;
+    MC.disabled = false;
+    MR.disabled = false;    
+  } else if (userInput === "memoryRecall") {
+    display.value = mem;
+    inserting = false;
+  } else if (userInput === "memoryClear") {
+    mem = 0;    
+    MC.disabled = true;
+    MR.disabled = true;
+  }
+}
+
+
+function handlerDisplay(e) {
+  display.value !== '0' 
+    ? display.value += e.target.value        
+    : display.value = e.target.value;    
+    //num = Number(display.value); 
+    //console.log(num);   
+  //return display.value;      
+}
 
 function toggleSign() {  
-  display.value = -display.value;  
+  display.value = -display.value;
+  /*num = Number(display.value);
+  console.log(num);
+  return num;  */
 }
 
 function setDecimal() {
   display.value.includes('.') ? '' : 
-    (display.value === '0' ||display.value === '')
+    (display.value === '0' || display.value === '')
       ? display.value = '0' + '.' 
-      : display.value = display.value + '.';
+      : display.value = display.value + '.';         
 }
 
-function handlerDisplay(e) {
-  display.value !== '0' 
-    ? display.value += e.target.id        
-    : display.value = e.target.id;
-  //return display.value; 
+function clearAll() {
+  n1 = null;   
+  n2 = null; 
+  operator = null;  
+  res = null;
+  display.value = 0;
 }
 
-function displayNumber() {
-  for (let number of numbers) {
-    if (number.id === 'sign') {
-      number.addEventListener("click", toggleSign);
-    } else if (number.id === 'dec') { 
-      number.addEventListener("click", setDecimal);
-    } else {
-      number.addEventListener("click", handlerDisplay);
-    }  
+function del() {
+  if (operator === null) {
+    if (isNaN(display.value)) {
+      return;
+    }    
+    if (display.value.length > 0) {
+      display.value = display.value.slice(0, -1);
+    }
+    if (display.value === '') {
+      display.value = "0";
+    }
+  } else {    
+    if (n2 === null) {
+      operator = null;
+    }
+    if (display.value.length > 0) {      
+      display.value = display.value.slice(0, -1);
+      n2 = Number(display.value);
+    }
+    if (display.value === '') {
+      display.value = "0";
+    }
+    console.log(n1, n2);
+  }  
+}
+
+function calculate(a, oper, b) {
+  if (oper === "divides" && n2 === 0) {
+    return "It's impossible!"
   }
- 
+  //calculated = true;
+  return operate[oper](a, b);  
 }
 
-displayNumber();
+function memStore() {
 
-//define variables:
-
-let n1, n2, operator;
-
-//define the operators and operation:
-
-function add(a, b) {
-  return a + b;
 }
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-
-//store the numbers and operate:
-
-let plus = document.querySelector('#adds');
-let minus = document.querySelector('#subtracts');
-let times = document.querySelector('#multiplies');
-let by = document.querySelector('#divides');
-let equal = document.querySelector('#equals');
-
-/*function setNumber(e) {
-  let num = +display.value;
-  (e.target === plus || e.target === minus || e.target === times || e.target === by) 
-    ? display.value = '0' : '';
-  return +num;  
-}*/
-
-function operate(operator, a, b) {
-  switch (operator) {
-    case 'adds':
-      return add(a, b);      
-    case 'subtracts':
-      return subtract(a, b);      
-    case 'multiplies':
-      return multiply(a, b);      
-    case 'divides':
-      return divide(a, b);          
-  }
-}
-
-function handlerOperations(e) {
-  n1 = +display.value;
-  //display.value = '';
-  //handlerDisplay(e);
-  console.log(n1);
-
-  console.log(e.target.id);
-  operator = e.target.id;
-  if (operator) {    
-    
-    n2 = displayNumber();
-  }
-  console.log(n2);
-  
-/*
-    n2 = operate(operator, n1, +display.value);
-    display.value = n1;
-    console.log(n1);
-    return n1;
-  } else {
-    return n1 = operate(operator, n1, +display.value)
-  }*/
-  
-  //n1 ? n1 = operate(operator, n1, setNumber(e)) : n1 = setNumber(e);
-  
-  
-  //n2 = setNumber(e);
-  //display.value = operate(e.target.id, n1, n2);
-    
-}
-
-plus.addEventListener('click', handlerOperations);
-minus.addEventListener('click', handlerOperations);
-times.addEventListener('click', handlerOperations);
-by.addEventListener('click', handlerOperations);
